70e9f9a7b00a09506e81d6dc021e14e1b7b916ef
AntonioPavlis <antonio.pavlis51@gmail.com>
2022-11-08 18:56:04 +0100

    Basic login/register

---
+++ SchedulerAPI/src/main/java/com/duty/scheduler/controllers/TestController.java
@@ -0,0 +1,46 @@
+package com.duty.scheduler.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duty.scheduler.models.Role;
+import com.duty.scheduler.payload.response.JwtResponse;
+import com.duty.scheduler.repository.RoleRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public String userAccess() {
+		return "User Content.";
+	}
+
+	@GetMapping("/mod")
+	@PreAuthorize("hasRole('MODERATOR')")
+	public String moderatorAccess() {
+		return "Moderator Board.";
+	}
+
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+}
--- WebScheduler/src/app/app.component.spec.ts
+++ WebScheduler/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WebScheduler'`, () => {
+  it(`should have as title 'AngularAuth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WebScheduler');
+    expect(app.title).toEqual('AngularAuth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WebScheduler app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularAuth app is running!');
   });
 });
