eeb092225e3cb3963a0356ac1b447722cf2469e6
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-11-20 20:25:08 +0100

    Configuration for SSL

--- WebScheduler/src/app/_services/app.settings.ts
+++ WebScheduler/src/app/_services/app.settings.ts
@@ -1,5 +1,5 @@
 
 export class AppSettings {
-    public static API_ENDPOINT = 'https://dutyschedulerapi-duty-scheduler-api.azuremicroservices.io/api/';
-    //public static API_ENDPOINT =  'http://localhost:180/api/';
+    //public static API_ENDPOINT = 'https://dutyschedulerapi-duty-scheduler-api.azuremicroservices.io/api/';
+    public static API_ENDPOINT =  'https://localhost:180/api/';
 }
