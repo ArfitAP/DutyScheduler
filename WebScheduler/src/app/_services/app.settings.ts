da45f8458653a84eeab34496ed5a18eed9eaafe2
AntonioPavlis <antonio.pavlis51@gmail.com>
2026-03-02 21:22:19 +0100

    Docker web

--- WebScheduler/src/app/_services/app.settings.ts
+++ WebScheduler/src/app/_services/app.settings.ts
@@ -1,7 +1,7 @@
 
 export class AppSettings {
     //public static API_ENDPOINT = 'https://dutyschedulerapi-duty-scheduler-api.azuremicroservices.io/api/';
-    //public static API_ENDPOINT = 'https://kt-apps-test.epcos.biz:8080/api/';
+    public static API_ENDPOINT = 'https://schedulerapi-bm12.onrender.com/api/';
     //public static API_ENDPOINT =  'http://localhost:180/api/';
-    public static API_ENDPOINT =  'http://localhost:1800/api/';
+    //public static API_ENDPOINT =  'http://localhost:1800/api/';
 }
